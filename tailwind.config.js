e124f540d0339b42521fb35ff79ff4af1fe1575a
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-30 19:19:36 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        rose: colors.rose,
+        pink: colors.pink,
+        fuchsia: colors.fuchsia,
+        purple: colors.purple,
+        violet: colors.violet,
+        indigo: colors.indigo,
+        blue: colors.blue,
+        sky: colors.sky,
+        cyan: colors.cyan,
+        teal: colors.teal,
+        emerald: colors.emerald,
+        green: colors.green,
+        lime: colors.lime,
+        yellow: colors.yellow,
+        amber: colors.amber,
+        orange: colors.orange,
+        red: colors.red,
+        slate: colors.slate,
+        zinc: colors.zinc,
+        gray: colors.gray,
+        neutral: colors.neutral,
+        stone: colors.stone,
+      }
+    },
+  },
+  plugins: [],
+}
