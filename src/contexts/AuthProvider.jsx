408d62be3bf2e05b8b4620ff5492d5c33c9fe149
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-31 15:41:04 +0600

    Implement AuthProvider with Firebase authentication integration
    - Created AuthProvider to manage authentication state using Firebase.
    - Set up user state and loading state to manage user data and loading status.
    - Added createUser function to register users with email and password.
    - Utilized onAuthStateChanged to listen for authentication state changes and update user state.
    - Included a cleanup function to unsubscribe from auth state changes on component unmount.
    - Provided authInfo context to children components for authentication handling.

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from "react";
+import app from "../utils/firebase.config";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+
+const authContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return unsubscribe;
+    }, []);
+
+
+    const authInfo = {
+        user,
+        setUser,
+        createUser
+    }
+    return (
+        <authContext.Provider value={authInfo}>
+            {children}
+        </authContext.Provider>
+    );
+};
+
+export default AuthProvider;
